41f3497c18778cfcc02bc4da8bb5cfa48ea67402
yanc0o <120708693+yanc0o@users.noreply.github.com>
2024-01-08 18:03:22 +0200

    Add files via upload

---
+++ Paint/Paint/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDrawing = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rbDraw = new System.Windows.Forms.RadioButton();
+            this.rbWrite = new System.Windows.Forms.RadioButton();
+            this.rbUppercase = new System.Windows.Forms.RadioButton();
+            this.rbLowercase = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelDrawing
+            // 
+            this.panelDrawing.Location = new System.Drawing.Point(2, 90);
+            this.panelDrawing.Name = "panelDrawing";
+            this.panelDrawing.Size = new System.Drawing.Size(612, 455);
+            this.panelDrawing.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(-4, 90);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(618, 447);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(294, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "eraser";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(6, 17);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(31, 29);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Blue;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(80, 52);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(31, 29);
+            this.button5.TabIndex = 10;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Fuchsia;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(80, 17);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(31, 29);
+            this.button6.TabIndex = 11;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Yellow;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(43, 52);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(31, 29);
+            this.button7.TabIndex = 12;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Black;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(6, 52);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(31, 29);
+            this.button8.TabIndex = 13;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Red;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Location = new System.Drawing.Point(43, 17);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(31, 29);
+            this.button10.TabIndex = 14;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "5px";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "10px";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rbDraw
+            // 
+            this.rbDraw.AutoSize = true;
+            this.rbDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDraw.Location = new System.Drawing.Point(241, 60);
+            this.rbDraw.Name = "rbDraw";
+            this.rbDraw.Size = new System.Drawing.Size(61, 24);
+            this.rbDraw.TabIndex = 15;
+            this.rbDraw.TabStop = true;
+            this.rbDraw.Text = "draw";
+            this.rbDraw.UseVisualStyleBackColor = true;
+            this.rbDraw.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbWrite
+            // 
+            this.rbWrite.AutoSize = true;
+            this.rbWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbWrite.Location = new System.Drawing.Point(308, 60);
+            this.rbWrite.Name = "rbWrite";
+            this.rbWrite.Size = new System.Drawing.Size(60, 24);
+            this.rbWrite.TabIndex = 16;
+            this.rbWrite.TabStop = true;
+            this.rbWrite.Text = "write";
+            this.rbWrite.UseVisualStyleBackColor = true;
+            this.rbWrite.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // rbUppercase
+            // 
+            this.rbUppercase.AutoSize = true;
+            this.rbUppercase.Location = new System.Drawing.Point(100, 61);
+            this.rbUppercase.Name = "rbUppercase";
+            this.rbUppercase.Size = new System.Drawing.Size(75, 17);
+            this.rbUppercase.TabIndex = 18;
+            this.rbUppercase.TabStop = true;
+            this.rbUppercase.Text = "uppercase";
+            this.rbUppercase.UseVisualStyleBackColor = true;
+            // 
+            // rbLowercase
+            // 
+            this.rbLowercase.AutoSize = true;
+            this.rbLowercase.Location = new System.Drawing.Point(12, 60);
+            this.rbLowercase.Name = "rbLowercase";
+            this.rbLowercase.Size = new System.Drawing.Size(73, 17);
+            this.rbLowercase.TabIndex = 19;
+            this.rbLowercase.TabStop = true;
+            this.rbLowercase.Text = "lowercase";
+            this.rbLowercase.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "2px";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Location = new System.Drawing.Point(495, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(119, 84);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(387, 40);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 549);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.rbLowercase);
+            this.Controls.Add(this.rbUppercase);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.rbWrite);
+            this.Controls.Add(this.rbDraw);
+            this.Controls.Add(this.panelDrawing);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDrawing;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton rbDraw;
+        private System.Windows.Forms.RadioButton rbWrite;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RadioButton rbUppercase;
+        private System.Windows.Forms.RadioButton rbLowercase;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Paint/Paint/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        private bool drawing = false;
+        private Point previousPoint;
+        private Pen currentPen = new Pen(Color.Black, 2);
+        public Form1()
+        {
+            InitializeComponent();
+            panelDrawing.MouseDown += PanelDrawing_MouseDown;
+            panelDrawing.MouseMove += PanelDrawing_MouseMove;
+            panelDrawing.MouseUp += PanelDrawing_MouseUp;
+
+        }
+    
+            private void PanelDrawing_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (rbDraw.Checked)
+            {
+                drawing = true;
+                previousPoint = e.Location;
+            }
+            else if (rbWrite.Checked)
+            {
+                richTextBox1.Focus();
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            }
+        }
+
+        private void PanelDrawing_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                using (Graphics g = panelDrawing.CreateGraphics())
+                {
+                    g.DrawLine(currentPen, previousPoint, e.Location);
+                    previousPoint = e.Location;
+                }
+            }
+        }
+
+        private void PanelDrawing_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawing = false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentPen.Color = panelDrawing.BackColor;
+            currentPen.Width = 20;
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            currentPen.Color = Color.White;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            currentPen.Color = Color.Red;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            currentPen.Color = Color.Fuchsia;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            currentPen.Color = Color.Black;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            currentPen.Color = Color.Yellow;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            currentPen.Color = Color.Blue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentPen.Width = 10;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentPen.Width = 5;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            currentPen.Width = 2;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            panelDrawing.Cursor = Cursors.Default;
+            richTextBox1.Visible = false;
+            panelDrawing.Visible = true;
+            rbLowercase.Visible = false;
+            rbUppercase.Visible = false;
+            groupBox1.Visible = true;
+            groupBox2.Visible = true;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            panelDrawing.Cursor = Cursors.IBeam;
+            panelDrawing.Visible = false;
+            richTextBox1.Visible = true;
+            rbLowercase.Visible = true;
+            rbUppercase.Visible = true;
+            groupBox1.Visible = false;
+            groupBox2.Visible = false;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (rbUppercase.Checked)
+            {
+                richTextBox1.Text = richTextBox1.Text.ToUpper();
+            }
+            else if (rbLowercase.Checked)
+            {
+                richTextBox1.Text = richTextBox1.Text.ToLower();
+            }
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+        }
+
+    }
+}
